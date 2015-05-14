e891230213f730dc7d60ca72a38d05bbc5717339
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-05-15 00:44:18 +0200

    Added main

---
+++ main.py
@@ -0,0 +1,29 @@
+#
+# main.py
+# Entry point for the game
+#
+# Jayant Shivarajan, Jonatan H Sundqvist
+# May 15 2015
+#
+
+# TODO | - 
+#        - 
+
+# SPEC | -
+#        -
+
+
+
+def main():
+
+	'''
+	Docstring goes here
+
+	'''
+
+	pass
+
+
+
+if __name__ == '__main__':
+	main()
