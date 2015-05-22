61a541dca87ee1a78f699d596bfc26b904e35587
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-05-22 20:37:59 +0200

    Created separate file for educational purposes

--- main.py
+++ main.py
@@ -21,35 +21,7 @@ def main():
 
 	'''
 
-	print(noIntegersPlease(5.2, 3+3j, 5e2j))
-
-
-
-def decorator(f):
-	def enhanced(*args, **kwargs):
-		assert all(not isinstance(arg, int) for arg in args)
-		print('Hurrah, no integers! I just hate them soo much! Haughty bastards...')
-		return f(*args, **kwargs)
-	return enhanced
-
-
-
-def prohibit(types):
-	def wrapper(f):
-		def restricted(*args, **kwargs):
-			for arg in args:
-				for t in types:
-					assert not isinstance(arg, t), 'Arrrgh, I hate {0}s!'.format(t)
-			return f(*args, **kwargs)
-		return restricted
-	return wrapper
-
-
-
-
-@prohibit((int, float, tuple, dict))
-def noIntegersPlease(*args):
-	return sum(args)
+	pass
 
 
 
